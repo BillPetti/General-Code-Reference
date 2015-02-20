9f38d953e830eaa65a3f63a307da0a3733997f40
Bill Petti <billpetti@gmail.com>
2015-02-20 06:57:28 -0500

    added weatherUnderground code as an option

--- weather_scrapping_crawling.R
+++ weather_scrapping_crawling.R
@@ -1,4 +1,6 @@
-#Crawling and scrappig weather data in R
+#Scraping weather data in R
+
+##Using rvest
 
 #install and load the necessary packages
 
@@ -35,6 +37,7 @@ ave_temp <- CH_weather_test %>%
     as.numeric()
 ave_temp
 
+##Create a set of urls for a series of dates at Weather Underground
 #set date range
 dates <- seq(as.Date("2007-05-20"), as.Date("2015-02-17"), by = "day")
 
@@ -46,10 +49,29 @@ datez <- format(dates, "%m/%d/%Y")
 
 urlz <- paste0("http://www.wunderground.com/history/airport/KVAY/", datez, "/DailyHistory.html?req_city=Cherry+Hill&req_state=NJ&req_statename=New+Jersey&reqdb.zip=08002&reqdb.magic=1&reqdb.wmo=99999&format=1")
 
-#Then use plyr to read each url and combine
+#Use plyr to read each url and combine
 
 df <- plyr::ldply(urlz)
 
 #rename url column to temp_urls
 colnames(df)<-"temp_urls"
 
+##Use the weatherData package to scrape data from Weather Underground (note: appears to be limited to 380 days of data at a time)
+
+#load packages
+library(weatherData)
+library(sqldf)
+
+#create data frames for dates of interest, by year
+yr_2007 <- getSummarizedWeather("KVAY", "2007-05-20", "2007-12-31")
+yr_2008 <- getSummarizedWeather("KVAY", "2008-01-01", "2008-12-31")
+yr_2009 <- getSummarizedWeather("KVAY", "2009-01-01", "2009-12-31")
+yr_2010 <- getSummarizedWeather("KVAY", "2010-01-01", "2010-12-31")
+yr_2011 <- getSummarizedWeather("KVAY", "2011-01-01", "2011-12-31")
+yr_2012 <- getSummarizedWeather("KVAY", "2012-01-01", "2012-12-31")
+yr_2013 <- getSummarizedWeather("KVAY", "2013-01-01", "2013-12-31")
+yr_2014 <- getSummarizedWeather("KVAY", "2014-01-01", "2014-12-31")
+yr_2015 <- getSummarizedWeather("KVAY", "2015-01-01", "2015-02-18")
+
+#combine all data frames into a single data frame
+all_dates<-rbind(yr_2007,yr_2008, yr_2009, yr_2010, yr_2011, yr_2012, yr_2013, yr_2014, yr_2015)
